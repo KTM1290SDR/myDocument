0379a3af431e6d7aec6edb050b3e5283b3579c09
xuyuxuan <1669194402@qq.com>
2021-03-10 13:47:03 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'External', link: 'https://google.com' },
+    ],
+    sidebar: [
+      ['/', '简介'],
+      // ['/cst/cst.md', '车商通'],
+      // ['/new/index1.md', '发布新框架'],
+      // ['/feedback/feedback.md', '问题反馈']
+  ]
+  }
+}
