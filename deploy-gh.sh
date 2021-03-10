991de3773ee315b42713369ee05a6ff2cedc0a9c
KTM1290DR <1669194402@qq.com>
2021-03-10 23:44:04 +0800

    feat

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:KTM1290SDR/myDocument.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:KTM1290SDR/myDocument.git main:gh-pages
+git push -f git@github.com:KTM1290SDR/myDocument.git master:gh-pages
 
 cd -
