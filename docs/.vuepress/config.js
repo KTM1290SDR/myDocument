16520d75cfc89f504e46d63e3099b80c2950f29d
KTM1290DR <1669194402@qq.com>
2021-03-11 20:19:09 +0800

    feat: 增加目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  title: '许宇轩的文档',
-  description: '技术文档',
-  base:"/myDocument/",
+  title: "许宇轩的文档",
+  description: "技术文档",
+  base: "/myDocument/",
   themeConfig: {
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'External', link: 'https://google.com' },
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "External", link: "https://google.com" },
     ],
     sidebar: [
-      ['/', '简介'],
-      ['/guide/', '引导'],
-      // ['/new/index1.md', '发布新框架'],
-      // ['/feedback/feedback.md', '问题反馈']
-  ]
-  }
-}
+      ["/", "简介"],
+      ["/guide/", "引导"],
+      ["/git", "git"],
+    ],
+  },
+};
