b0e20de207db361387273d719d1ba28438541cc4
KTM1290DR <1669194402@qq.com>
2021-11-18 00:34:20 +0800

    deploy commit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,51 +11,59 @@ module.exports = {
     sidebar: [
       ["/", "简介"],
       {
-        title: "算法和数据结构",
-        path: "/algorithmAndDataStructure/",
+        title: "JavaScript",
+        path: "/JavaScript/",
         sidebarDepth: 1,
         children: [
-          {
-            title: "算法",
-            path: "/algorithmAndDataStructure/algorithm/",
-            sidebarDepth: 2,
-          },
-          {
-            title: "数据结构",
-            path: "/algorithmAndDataStructure/dataStructure/",
-            sidebarDepth: 2,
-          },
-          {
-            title: "leetCode",
-            path: "/algorithmAndDataStructure/leetCode/",
-            sidebarDepth: 2,
-          },
+
         ],
       },
-      {
-        title: "git",
-        path: "/git/",
-        sidebarDepth: 1,
-        children: [
-          {
-            title: "引导",
-            path: "/guide/",
-            sidebarDepth: 2,
-            children: ["/"],
-          },
-        ],
-      },
-      {
-        title: "vue3-vite-ts",
-        path: "/vue3ViteTs/",
-        sidebarDepth: 1,
-        children: ["/vue3ViteTs/"],
-      },
-      {
-        title: "其他",
-        path: "/other/",
-        sidebarDepth: 1,
-      },
+      // {
+      //   title: "算法和数据结构",
+      //   path: "/algorithmAndDataStructure/",
+      //   sidebarDepth: 1,
+      //   children: [
+      //     {
+      //       title: "算法",
+      //       path: "/algorithmAndDataStructure/algorithm/",
+      //       sidebarDepth: 2,
+      //     },
+      //     {
+      //       title: "数据结构",
+      //       path: "/algorithmAndDataStructure/dataStructure/",
+      //       sidebarDepth: 2,
+      //     },
+      //     {
+      //       title: "leetCode",
+      //       path: "/algorithmAndDataStructure/leetCode/",
+      //       sidebarDepth: 2,
+      //     },
+      //   ],
+      // },
+      // {
+      //   title: "git",
+      //   path: "/git/",
+      //   sidebarDepth: 1,
+      //   children: [
+      //     {
+      //       title: "引导",
+      //       path: "/guide/",
+      //       sidebarDepth: 2,
+      //       children: ["/"],
+      //     },
+      //   ],
+      // },
+      // {
+      //   title: "vue3-vite-ts",
+      //   path: "/vue3ViteTs/",
+      //   sidebarDepth: 1,
+      //   children: ["/vue3ViteTs/"],
+      // },
+      // {
+      //   title: "其他",
+      //   path: "/other/",
+      //   sidebarDepth: 1,
+      // },
     ],
   },
 };
